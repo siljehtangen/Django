69d67a753c3159456ce4655424363d2da4dae4d5
Silje Hanø Tangen <Siljehtangen@outlook.com>
2022-10-13 18:22:59 +0200

    Feridg med lesson 8

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return self.title
